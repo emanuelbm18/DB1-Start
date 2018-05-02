9a915dd1eb1d0d6f505b6a0cfa952a7e1078053f
emanuelbm18 <38914597+emanuelbm18@users.noreply.github.com>
2018-05-02 10:30:33 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, Perl, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    {
+        cout<<("Digite um número\n");
+        cin>>(n);
+        if (n%2==0)//Condição "É PAR"
+        {
+            cout<<"É PAR\n";
+        }
+        else//Condição do "if" acima
+        {
+            cout<<"Não é PAR\n";
+        }
+        if (n%2!=0)//Condição "É IMPAR"
+        {
+            cout<<"É IMPAR\n";
+        }
+        else//Condição do"if" acima
+        {
+            cout<<"Não é IMPAR\n";
+        }
+        if (n>10)//Condição "Maior que 10"
+        {
+            cout<<"É maior que 10\n";
+        }
+        else//Condição do "if" acima
+        {
+            cout<<"Não é maior que 10\n";
+        }
+        cout<<"Valor dobrado do valor "<<n<<" é: "<<(n*2)<<"\n";
+        if (n%n==1 && n%1==n)
+        {
+            cout<<"É PRIMO\n";
+        }
+        else
+        {
+            cout<<"Não é PRIMO\n";
+        }
+    }
+}
